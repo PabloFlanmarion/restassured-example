77765962fa0af84bbd460b91fd942f97e9006918
PabloFLanmarion <pabloflanmarion@gmail.com>
2020-07-28 23:51:20 -0300

    First Commit

---
+++ src/main/java/com/exemple/RestassuredExempleApplication.java
@@ -0,0 +1,13 @@
+package com.exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestassuredExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestassuredExempleApplication.class, args);
+	}
+
+}
